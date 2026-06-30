51b5d5c6be9e29db333d7d458b0c3406325b7488
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2026-06-30 22:13:36 +0100

    CopyMultipleReaders

--- src/Microsoft.Data.SqlClient/tests/ManualTests/BulkCopy/CopyMultipleReaders.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/BulkCopy/CopyMultipleReaders.cs
@@ -4,6 +4,7 @@
 
 using System.Data.Common;
 using Microsoft.Data.SqlClient.ManualTesting.Tests;
+using Microsoft.Data.SqlClient.Tests.Common.Fixtures.DatabaseObjects;
 using Xunit;
 
 namespace Microsoft.Data.SqlClient.ManualTests.BulkCopy
@@ -16,16 +17,13 @@ namespace Microsoft.Data.SqlClient.ManualTests.BulkCopy
         {
             string srcConstr = DataTestUtility.TCPConnectionString;
             string dstConstr = DataTestUtility.TCPConnectionString;
-            string dstTable = DataTestUtility.GetShortName("SqlBulkCopyTest_CopyMultipleReaders", false);
             using (SqlConnection dstConn = new SqlConnection(dstConstr))
             using (SqlCommand dstCmd = dstConn.CreateCommand())
             {
                 dstConn.Open();
 
-                try
+                using (Table dstTable = new(dstConn, nameof(CopyMultipleReaders), "(col1 int, col2 nvarchar(20), col3 nvarchar(10))"))
                 {
-                    Helpers.TryExecute(dstCmd, "create table " + dstTable + " (col1 int, col2 nvarchar(20), col3 nvarchar(10))");
-
                     using (SqlConnection srcConn = new SqlConnection(srcConstr))
                     using (SqlCommand srcCmd = srcConn.CreateCommand())
                     {
@@ -33,7 +31,7 @@ namespace Microsoft.Data.SqlClient.ManualTests.BulkCopy
 
                         using (SqlBulkCopy bulkcopy = new SqlBulkCopy(dstConn))
                         {
-                            bulkcopy.DestinationTableName = dstTable;
+                            bulkcopy.DestinationTableName = dstTable.Name;
                             srcCmd.CommandText = "select EmployeeID, LastName from employees where LastName < 'E%'";
                             using (DbDataReader reader = srcCmd.ExecuteReader())
                             {
@@ -55,13 +53,9 @@ namespace Microsoft.Data.SqlClient.ManualTests.BulkCopy
                             }
                             DataTestUtility.AssertEqualsWithDescription(0, bulkcopy.ColumnMappings.Count, "Unexpected ColumnMappings count.");
 
-                            Helpers.VerifyResults(dstConn, dstTable, 3, 15);
+                            Helpers.VerifyResults(dstConn, dstTable.Name, 3, 15);
                         }
                     }
-                }
-                finally
-                {
-                    Helpers.TryExecute(dstCmd, "drop table " + dstTable);
                 }
             }
         }
